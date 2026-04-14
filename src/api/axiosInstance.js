6df45934737601b8a5e9ac865c1c9cdaedfa4949
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-04-14 21:03:29 +0530

    token

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -17,8 +17,11 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // Assuming your token is stored in a cookie named 'token'
-    const token = getCookie('token');
-    
+    // const token = getCookie('token');
+
+    // Get token from localStorage natively or fallback to cookie
+    const token = localStorage.getItem('token') || getCookie('token');
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // Or however your backend expects it
     }
