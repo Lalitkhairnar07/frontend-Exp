d9b5654f186b24294f7c0d80f4573090b60ead5f
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-06-05 17:02:14 +0530

    routes

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -17,7 +17,7 @@ const AppRoutes = () => {
     
     const router = createBrowserRouter([
         {
-            path: "/",
+            path: "/login",
             element:<Login/>
         },
         {
@@ -25,7 +25,7 @@ const AppRoutes = () => {
             element: <ForgotPassword />
         },
         {
-            path:"/user",
+            path:"",
             element:<UserNavbar/>,
             children:[
                 {
